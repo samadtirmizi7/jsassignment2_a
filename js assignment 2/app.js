d4d2880c648ed72b08fa12cf72d1f7980da0dfea
Syed Abdul Samad Tirmizi <144122288+samadtirmizi7@users.noreply.github.com>
2023-09-13 11:21:13 +0500

    Add files via upload

---
+++ js assignment 2/app.js
@@ -0,0 +1,159 @@
+// Task # 1 :
+{
+// var userCity = prompt("Enter the city Name...");
+// if (userCity === "karachi"){
+// console.log("“Welcome to city of lights”")}
+// else{
+//     console.log("please enter karachi...")
+// }
+}
+// Task # 2
+{
+// var userGender = prompt("Please enter your Gender...");
+// if (userGender === "male"){
+//     console.log("Good Morning Sir..");
+// }
+// else if (userGender === "female"){
+//     console.log("Good Morning Maam.");
+// }
+// else{
+//     console.log("please enter male or female..");
+// }
+}   
+// Task # 3
+{
+    // var userGender = prompt("Enter traffic lights Red Yellow OR Green");
+    // if (userGender === "red"){
+    //     console.log("Must Stop");
+    // }
+    // else if (userGender === "yellow"){
+    //     console.log("Ready to move");
+    // }
+    // else if (userGender === "green"){
+    //     console.log("Move Now");
+    // }
+    // else{
+    //     console.log("please enter correct !");
+    // }   
+}
+
+// Task # 4
+{
+// var carFuel = +prompt("Enter remaining fuel in Liters","e.g = 2.5 liters");
+// if (carFuel <= 0.25){
+//     console.log("“Please refill the fuel in your car”" +" because"+ " " + carFuel +" liters is left...")
+// }
+// else {
+//     console.log("You have enough fuel in your car.");
+// }
+}
+
+// Task # 5
+{
+// a)it is running and showing alert where it is showing "given condition for variable a is true"
+// b)Not Running
+// c)it is running and showing alert where it is showing "condition 2 is true and condition 4 is true"
+// d)it is running and showing alert where it is showing "The cost equals"
+// e)it is running and showing alert where it is showing "True"
+// f)it is running and showing alert where it is showing "car is smaller than cat"
+ 
+}
+// Task # 6
+{
+// var FirstSubject = +prompt("Enter first subject marks");
+// var SecondSubject = +prompt("Enter Second subject marks");
+// var ThirdSubject = +prompt("Enter Third subject marks");
+// var TotalMarks = +prompt("Enter the Total Marks of all subjects");
+// var ObtainMarks = FirstSubject + SecondSubject + ThirdSubject
+// var Percentage = ObtainMarks / TotalMarks * 100  
+
+// if (Percentage >= 80)
+// document.write ("<b>MARKSHEET <br> <br> <br>" +"TotalMarks: " + TotalMarks + "<br>" + "ObtainMarks:" + ObtainMarks + "<br>" + "Percentage:" + Percentage + "<br>" + "Grade: A-one <br>" + "Remarks: Excellent."  )
+
+// else if (Percentage >= 70)
+// document.write ("<b>MARKSHEET <br> <br> <br>" +"TotalMarks: " + TotalMarks + "<br>" + "ObtainMarks:" + ObtainMarks + "<br>" + "Percentage:" + Percentage + "<br>" + "Grade: A <br>" + "Remarks: Good."  )
+
+// else if (Percentage >= 60)
+// document.write ("<b>MARKSHEET <br> <br> <br>" +"TotalMarks: " + TotalMarks + "<br>" + "ObtainMarks:" + ObtainMarks + "<br>" + "Percentage:" + Percentage + "<br>" + "Grade: B <br>" + "Remarks: You need to improve."  )
+
+// else if (Percentage < 60 )
+// document.write ("<b>MARKSHEET <br> <br> <br>" + "TotalMarks: " + TotalMarks + "<br>" + "ObtainMarks:" + ObtainMarks + "<br>" + "Percentage:" + Percentage + "<br>" + "Grade: Fail <br>" + "Remarks: Sorry.")
+// }
+// else {
+//     document.write("Enter correct Numbers...");
+}
+
+// Task # 7
+// {
+// var secretNumber = 7;
+// var userInput = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+// if (userInput === secretNumber) {
+//     console.log("Bingo! Correct answer.");
+// } else if (userInput === secretNumber + 1 ) {
+//     console.log("Close enough to the correct answer.");
+// } else {
+//     console.log("Sorry, that's not correct.");
+// }
+// }
+
+// Task # 8
+// {
+// var userNumber = +(prompt("Enter a number:"));
+// if (userNumber % 3 === 0) {
+//     console.log(userNumber + " " + "is divisible by 3.");
+// } else {
+//     console.log(userNumber + " " + "is not divisible by 3.");
+// }
+// }
+
+// Task # 9 
+// {
+// var userNum = parseInt(prompt("Enter a number:"));
+// if (userNum % 2 === 0) {
+//     console.log(userNum + " is an even number.");
+// } else {
+//     console.log(userNum + " is an odd number.");
+// }
+// }
+
+// Task # 10
+// {
+// var inputTemp = +(prompt("Enter the temperature:"));
+// if (inputTemp > 40) {
+//     console.log("It is too hot outside.");
+// } else if (inputTemp > 30) {
+//     console.log("The Weather today is Normal.");
+// } else if (inputTemp > 20) {
+//     console.log("Today’s Weather is cool.");
+// } else if (inputTemp > 10) {
+//     console.log("OMG! Today’s weather is so Cool.");
+// } else {
+//     console.log("It is very cold outside.");
+// }
+// }
+
+// Task # 11
+// {
+// var firstNum = +prompt("enter first number");
+// var secondNum = +prompt("enter Second number");
+// var operaTion = prompt("enter any of one Operation...(+, -, *, /, %)");
+
+// if (operaTion === "+"){
+//     console.log(firstNum + secondNum);
+// }
+// else if (operaTion === "-"){
+//     console.log(firstNum - secondNum);
+// }
+// else if (operaTion === "*"){
+//     console.log(firstNum * secondNum);
+// }
+// else if (operaTion === "/"){
+//     console.log(firstNum / secondNum);
+// }
+// else if (operaTion === "%"){
+//     console.log(firstNum % secondNum);
+// }
+// else {
+//     console.log("please enter correct sign..");
+// }
+// }
